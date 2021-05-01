5921b94e14a2f9573b18069dd1fecac932b6a302
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-01 10:47:28 +0530

    haeder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './app.scss';
-
 import React from 'react'
+import Header from './components/Header';
+import './styles/app.scss';
 
 const App = () => {
     return (
-        <div>
-            hulu
+        <div className="app">
+            <Header />
         </div>
     )
 }
