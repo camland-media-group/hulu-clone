314784a6378fcf967fe58d677b9bb10a9d185954
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-02 21:40:10 +0530

    fetching the movie data and adding to videocard

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import VideoCard from './VideoCard'
+import axios from '../api/axios'
+import requests from '../api/requests'
 
 const Results = () => {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        const fetchData = async() => {
+            const request = await axios.get(requests.fetchTrending)
+
+            console.log(request.data.results);
+
+            setMovies(request.data.results)
+
+            return request
+        }
+
+        fetchData()
+    }, [])
+
     return (
         <div className="results">
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
+        {movies.map(movie => (
+            <VideoCard key={movie.id} movie={movie} />
+        ))}
         </div>
     )
 }
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import TextTruncate from 'react-text-truncate'
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+
+const posterURL = 'https://image.tmdb.org/t/p/original/'
 
 const VideoCard = ({ movie }) => {
     return (
         <div className="videocard">
-            <img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/2338453/settings_images/ke9mPRWnQkef1SSP2Vhn_React_Netflix_Clone.png" alt="img"/>
-            <p>Movie desc</p>
-            <h2>Movie title</h2>
-            <p>number of likes</p>
+            <div className="videocard__img">
+                <img 
+                    src={`${posterURL}${movie.backdrop_path || movie.poster_path}`} 
+                    alt={movie.title}
+                />
+            </div>
+            <TextTruncate
+                line={1}
+                element="p"
+                text={movie.overview}
+                truncateText="..."
+                // textTruncateChild={<a href="#">Read More</a>}
+            />
+            <h2>{movie.title || movie.original_name}</h2>
+            <p className="videocard__stats">
+                <span>{movie.media_type && `${movie.media_type} • `} </span>
+                <span>{movie.release_date || movie.first_air_date} • </span>
+                <span><ThumbUpIcon /></span>
+                <span>{movie.vote_count}</span>
+            </p>
         </div>
     )
 }
